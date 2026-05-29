33dbc192398417d04f89bf822b54b1e5e5655c42
Federico Caselli <cfederico87@gmail.com>
2026-05-29 08:57:23 +0200

    update example tokens in the example files
    also note that a real token should have smaller expiry dates

--- examples/readme_example_async.py
+++ examples/readme_example_async.py
@@ -17,7 +17,7 @@ from falcon_auth2.backends import MultiAuthBackend
 # uvicorn --port 8080 readme_example_async:app
 # You can then use httpie to interact with it. Example
 # http :8080/hello -a foo:bar
-# http :8080/hello 'Authorization:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiYXIiLCJpc3MiOiJhbiBpc3N1ZXIiLCJhdWQiOiJhbiBhdWQiLCJpYXQiOjE2MDk0NTkyMDAsIm5iZiI6MTYwOTQ1OTIwMCwiZXhwIjoxOTI0OTkyMDAwfQ.FDpE_-jL-reHrheIYVGbwdVf8g1HWFAoGJet_6zC2Tk'
+# http :8080/hello 'Authorization:Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiYXIiLCJpc3MiOiJhbiBpc3N1ZXIiLCJhdWQiOiJhbiBhdWQiLCJpYXQiOjE2MDk0NTkyMDAsIm5iZiI6MTYwOTQ1OTIwMCwiZXhwIjoxOTI0OTkyMDAwfQ.-5n2vQl3ejYeHQFq6NyFFwTncya780S1BDlPKgffBMQ'
 # http :8080/no-auth
 # http POST :8080/generic User:foo
 
@@ -119,6 +119,6 @@ def make_token() -> None:
         "aud": "an aud",
         "iat": 1609459200,
         "nbf": 1609459200,
-        "exp": 1924992000,
+        "exp": 1924992000,  # a real token should have a shorter exp time
     }
     print(jwt.encode({"alg": "HS256"}, payload, key))
